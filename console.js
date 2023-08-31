de75676f37900784aa48f6f91aa58bcc8d17e807
David Fink <davidfink3@gmail.com>
2023-08-31 14:58:41 -0400

    Created entire game function.

--- console.js
+++ console.js
@@ -1,12 +1,49 @@
 console.log("Hello World");
 
 function getComputerChoice(){
-    var plays = ["Rock","Paper","Scissors"];
+    var plays = ["rock","paper","scissors"];
     var ran = Math.floor(Math.random()*3);
-    console.log(plays[ran]);
     return plays[ran];
 }
 
+function playGame(playerSelection){
+    var computerChoice = getComputerChoice();
+    console.log(playerSelection);
+    console.log(computerChoice);
+    if(playerSelection === "rock"){
+        if(computerChoice === "scissors"){
+            return "You win! Rock beats scissors";
+        }
+        else if(computerChoice === "paper"){
+            return "You lose! Paper beats rock";
+        }
+        else{
+            return "Tie!"
+        }
+    }
+    if(playerSelection === "scissors"){
+        if(computerChoice === "paper"){
+            return "You win! Scissors beats rock";
+        }
+        else if(computerChoice === "rock"){
+            return "You lose! Rock beats scissors";
+        }
+        else{
+            return "Tie!"
+        }
+    }
+    if(playerSelection === "paper"){
+        if(computerChoice === "rock"){
+            return "You win! Paper beats rock";
+        }
+        else if(computerChoice === "scissors"){
+            return "You lose! Scissor beats paper";
+        }
+        else{
+            return "Tie!"
+        }
+    }   
+}
 
-getComputerChoice();
-console.log(Math.floor(Math.random()*3));
+var str = playGame("paper");
+console.log(str);
